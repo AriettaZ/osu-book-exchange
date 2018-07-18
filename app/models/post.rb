8b3bd30cb5faf8b8ca7c605bc94a2cc81d992652
Gail Chen <chentianzhikid@gmail.com>
2018-07-17 22:07:35 -0400

    update seed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :book
-  # belongs_to :user
+  belongs_to :user
   enum type: {offer: 0, request: 1}
   enum payment_method: {inperson: 0, online: 1}
   enum status: {active: 0, pending: 1, closed: 2}
