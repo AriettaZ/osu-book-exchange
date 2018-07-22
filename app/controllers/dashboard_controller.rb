c04a49ffc24afacdd709362181f53f7d17df8f36
Yuxiang Lin <lin.2453@osu.edu>
2018-07-22 19:50:05 -0400

    Add Message Function

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -11,7 +11,72 @@ class DashboardController < ApplicationController
   def account_information
   end
 
+  def contacts
+    contacts_id = {}
+    Message.where(sender_id: current_user.id).find_each do |message|
+      unless contacts_id.key?(message.receiver_id) then
+        unless contacts_id[message.receiver_id]==message.post_id then
+          contacts_id[message.receiver_id]=message.post_id
+        end
+      end
+    end
+
+    Message.where(receiver_id: current_user.id).find_each do |message|
+      unless contacts_id.key?(message.sender_id) then
+        unless contacts_id[message.sender_id]==message.post_id then
+          contacts_id[message.sender_id]=message.post_id
+        end
+      end
+    end
+
+    @contacts = {}
+    contacts_id.each do |user_id, post_id|
+      @contacts[User.find_by_id(user_id)]=Post.find_by_id(post_id)
+    end
+    @talk_to = User.new
+  end
+
   def messages
+    # @messages = Message.where(sender_id: 2, receiver_id: 12)
+    @messages = []
+    Message.where(sender_id: current_user.id, receiver_id: params[:talk_to]).find_each do |message|
+      @messages.append(message)
+    end
+    Message.where(sender_id: params[:talk_to], receiver_id: current_user.id).find_each do |message|
+      @messages.append(message)
+    end
+
+    @messages.sort_by! do |message|
+      message.created_at
+    end
+    @msg = params
+    @message = Message.new
+
+  end
+
+  def create_message
+    # if !Post.find_by_id(params[:post_id])
+    #   render "messages/new" and return
+    # end
+
+    @message = Message.new(content: params[:message][:content],
+                           sender_id: current_user.id,
+                           # receiver_id: Post.find(params[:post_id]).user.id
+                           post_id: 1,
+                           receiver_id: 2
+              )
+    # flash.now[:success] = @message.inspect
+    if @message.save
+      flash[:success] = "Message sent for post id: " + params[:post_id].to_s
+      redirect_to dashboard_messages_path(talk_to: params[:talk_to])
+    else
+      # Show saving errors.
+      @message.errors.each do |type, text|
+        flash.now[:success] = type.to_s.capitalize + " " + text
+      end
+      redirect_to dashboard_messages_path(talk_to: params[:talk_to])
+    end
+
   end
 
   def bookmarks
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get 'dashboard/myrequest'
   get 'dashboard/myoffer'
   get 'dashboard/account_information'
-  get 'dashboard/messages'
+  get 'dashboard/messages', to: 'dashboard#messages'
+  post 'dashboard/messages', to: 'dashboard#create_message'
+  get 'dashboard/contacts', to: 'dashboard#contacts'
   get 'dashboard/bookmarks'
   devise_for :users, path: '', path_names:{sign_in: 'login', sign_out: 'logout' , sign_up: 'signup'}
   resources :orders
@@ -17,5 +19,6 @@ Rails.application.routes.draw do
   resources :images
   root to: "pages#home"
   resources :messages, except: [:edit, :update, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
