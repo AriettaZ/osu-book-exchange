4efd409ddaf6b9a7608ac432701502673ef9559a
ariel <zhu.1444@osu.edu>
2018-07-22 20:13:31 -0400

    change nav

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -48,18 +48,3 @@ $(document).ready(function(){
 
   });
 })
-// $("#book-search-box").bind("keyup", function() {
-//
-//   $("#book-search-box").addClass("loading"); // show the spinner
-//   var form = $("#live-search-form"); // grab the form wrapping the search bar.
-//
-//   var url = "https://www.googleapis.com/books/v1/volumes?"+form.serialize(); // live_search action.
-//   $.get(url,function(html) { // perform an AJAX get
-//
-//     $("#book-search-box").removeClass("loading"); // hide the spinner
-//
-//     $("#live-search-results").html(html); // replace the "results" div with the results
-//
-//   });
-//
-// });
