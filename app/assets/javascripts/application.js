89763f8e5e882d2a3a280405a0eee1fa0bb3dfec
Channing Jacobs <channingjacobs@gmail.com>
2018-07-24 05:36:59 -0400

    Updated bookmarks.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,11 +21,10 @@
 //= require popper
 //= require bootstrap-sprockets
 $(document).ready(function(){
-  $("select, :text, textarea").addClass("form-control")
-  $("select").addClass("form-control")
-  $(":input").not(":submit").addClass("form-control")
+  $("select, :text, textarea").addClass("form-control");
+  $("select").addClass("form-control");
+  $(":input").not(":submit").addClass("form-control");
   $(".actual_product_image").each(function(){
     $(this).on('load', $(this).parent().attr("data-lightbox", "roadtrip"));
-  })
   });
-})
+});
