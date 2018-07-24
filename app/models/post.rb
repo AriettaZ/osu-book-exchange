fd0f07e3ee9280a5899cdc6bfeafd3fb23777837
Channing Jacobs <channingjacobs@gmail.com>
2018-07-23 20:05:42 -0400

    Updated README and bug fixes.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :images
   has_many :bookmarks
+  # Also has many :users, through: :bookmarks
   accepts_nested_attributes_for :images
   has_many :messages
   has_many :contracts
