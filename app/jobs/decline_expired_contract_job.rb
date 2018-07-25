b033c638a7f4e46bb1d7df86e1596dc08fc175ce
Gail Chen <chentianzhikid@gmail.com>
2018-07-25 12:07:34 -0400

    disable online payment

--- app/jobs/decline_expired_contract_job.rb
+++ app/jobs/decline_expired_contract_job.rb
@@ -1,6 +1,7 @@
 class DeclineExpiredContractJob < ApplicationJob
   queue_as :default
 
+  # If the contract is expired and it is not confirmed or declined, then decline the contract and send email notifications to users.
   def perform(contract, signed_user, unsigned_user)
     post = Post.find(contract.post_id)
 
