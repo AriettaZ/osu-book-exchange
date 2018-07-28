68ccaf4bd0d2a57e77e81b8d0a9f803796a619e3
Yuxiang Lin <lin.2453@osu.edu>
2018-07-27 20:40:15 -0400

    Add documentation

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -44,10 +44,13 @@ class DashboardController < ApplicationController
     redirect_to edit_user_registration_url
   end
 
+  # Author: Mike
+  # Created: 7/19
+  # Description: GET route for retrieving conversations
   def contacts
     @contacts = []
 
-
+    # Search current_user as sender
     Message.where(sender_id: current_user.id).find_each do |message|
       has_contact = false
       @contacts.each do |contact|
@@ -57,11 +60,11 @@ class DashboardController < ApplicationController
           if (contact.latest_message_time<=>message.created_at)<0 then
             contact.latest_message_time = message.created_at
           end
-
           has_contact = true
         end
       end
 
+      # If haven't added partner to list contact, then create and append the new contact
       unless has_contact then
         new_contact = Contact.new(message.receiver_id, User.find_by_id(message.receiver_id) , message.post_id, message.created_at)
         new_contact.post = Post.find_by_id(message.post_id)
@@ -70,11 +73,9 @@ class DashboardController < ApplicationController
       end
     end
 
-
+    # Search current_user as receiver
     Message.where(receiver_id: current_user.id).find_each do |message|
-
       has_contact = false
-
       @contacts.each do |contact|
         # If find partner and post match
         if contact.partner_id==message.sender_id && contact.post_id==message.post_id then
@@ -82,11 +83,11 @@ class DashboardController < ApplicationController
           if (contact.latest_message_time<=>message.created_at)<0 then
             contact.latest_message_time = message.created_at
           end
-
           has_contact = true
         end
       end
 
+      # If haven't added partner to list contact, then create and append the new contact
       unless has_contact then
         new_contact = Contact.new(message.sender_id, User.find_by_id(message.sender_id) , message.post_id, message.created_at)
         new_contact.post = Post.find_by_id(message.post_id)
@@ -95,55 +96,60 @@ class DashboardController < ApplicationController
       end
     end
 
-
+    # Sort the conversation by message time
     @contacts.sort! do |b, a|
       a.latest_message_time <=> b.latest_message_time
     end
-
     @talk_to = User.new
   end
 
+
+  # Author: Mike
+  # Created: 7/19
+  # Description: Retrive all the message about a conversation in the message database
   def messages
     @messages = []
+
+    # If has parameter last_msg_time, then use the method append_messages
     if params[:last_msg_time] && params[:last_msg_time]!="undefined" then
       append_messages
       return
     else
-      # @messages = Message.where(sender_id: 2, receiver_id: 12)
+      # Search Message for current_user as sender
       Message.where(sender_id: current_user.id, receiver_id: params[:talk_to]).find_each do |message|
         @messages.append(message)
       end
+      # Search Message for current_user as receiver
       Message.where(sender_id: params[:talk_to], receiver_id: current_user.id).find_each do |message|
         @messages.append(message)
       end
+      # Sort the result messages by time
       @messages.sort_by! do |message|
         message.created_at
       end
-      # @msg = params
       @talk_to_name = User.find_by_id(params[:talk_to]).name
       @message = Message.new
     end
 
   end
 
+  # Author: Mike
+  # Created: 7/19
+  # Description: POST route to create a new message
   def create_message
     @message = Message.new(content: params[:content],
                            sender_id: current_user.id,
-                           # receiver_id: Post.find(params[:post_id]).user.id
                            post_id: params[:post_id].to_i,
                            receiver_id: params[:talk_to].to_i
               )
-    # flash.now[:success] = @message.inspect
     if @message.save
       flash[:success] = "Message sent for post id: " + params[:post_id].to_s
-      # redirect_to profile_messages_path(talk_to: params[:talk_to], post_id: params[:post_id])
       if params[:last_msg_time] && params[:last_msg_time]!="undefined" then
         append_messages
       else
         redirect_to profile_messages_path(talk_to: params[:talk_to], post_id: params[:post_id])
       end
     else
-      # Show saving errors.
       @message.errors.each do |type, text|
         flash.now[:success] = type.to_s.capitalize + " " + text
       end
@@ -152,22 +158,31 @@ class DashboardController < ApplicationController
 
   end
 
+  # Author: Mike
+  # Created: 7/19
+  # Description: Retrive messages since last_msg_time
   def append_messages
       @appended_messages = []
+
+      # Retrive messages for current_user as sender
       Message.where(created_at: params[:last_msg_time]..Time.now, sender_id: current_user.id, receiver_id: params[:talk_to]).find_each do |message|
         @appended_messages.append(message)
       end
 
+      # Retrive messages for current_user as receiver
       Message.where(created_at: params[:last_msg_time]..Time.now, sender_id: params[:talk_to], receiver_id: current_user.id).find_each do |message|
         @appended_messages.append(message)
       end
 
+      # Sort the messages by time
       @appended_messages.sort_by! do |message|
         message.created_at
       end
 
+      # Remove the first one for duplicates
       @appended_messages.shift
 
+      # Render the partial html for Ajax call
       render partial: "appended_messages"
 
   end
