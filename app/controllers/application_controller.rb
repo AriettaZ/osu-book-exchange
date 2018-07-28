5814d2c84a98a53d2f06cc0ac44209fdfb624e62
ariel <zhu.1444@osu.edu>
2018-07-27 20:23:13 -0400

    add include to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
-  include DeviseWhitelist
-  include CurrentUser
-
+  devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
+  devise_parameter_sanitizer.permit(:account_update,keys:[:name])
+  devise_parameter_sanitizer.permit(:sign_up,keys:[:major])
+  devise_parameter_sanitizer.permit(:account_update,keys:[:major])
+  def current_user
+    super || guest_user
+  end
+  def guest_user
+    guest=GuestUser.new
+    guest.name = "Guest"
+    guest.first_name="Guest"
+    guest.last_name="User"
+    guest.email="Guest@example.com"
+    guest.roles=[:guest]
+    guest
+  end
 end
